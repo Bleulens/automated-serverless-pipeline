d3a5a50b02e23cff875a726f268cdb7dfb8ff424
Bleulens <popman90@gmail.com>
2025-12-20 13:35:29 -0500

    Add s3ReadError and S3WriteError classes to errors.py

--- lambda_function/errors.py
+++ lambda_function/errors.py
@@ -2,3 +2,15 @@ class TransformError(Exception):
     """Custom exception for transform-related errors."""
 
     pass
+
+
+class S3ReadError(Exception):
+    """Custom exception for S3 read errors."""
+
+    pass
+
+
+class S3WriteError(Exception):
+    """Custom exception for S3 write errors."""
+
+    pass
